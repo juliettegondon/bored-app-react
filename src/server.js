ae48b51d52b17f8da52d6d324b75dd7d3f46f639
Thomas Siller <siller.thomas@gmx.net>
2019-01-30 00:50:06 +0100

    add server log

--- src/server.js
+++ src/server.js
@@ -3,4 +3,7 @@ var express = require('express')
 var app = express()
 app.use(express.static('public'))
 
-app.listen(process.env.PORT || 8080)
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+  console.log(`Server running on Port ${port}!`) // eslint-disable-line no-console
+})
